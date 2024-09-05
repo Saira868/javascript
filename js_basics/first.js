f7932002ed1f4806c4be7377658133c1cce66fd1
Saira868 <137001691+Saira868@users.noreply.github.com>
2024-09-05 09:07:49 +0000

    js datatypes

---
+++ js_basics/first.js
@@ -0,0 +1,44 @@
+// const studentId = 12345
+
+// console.log(studentid);
+// datatypes 
+//primitive string , numbers , undefined , boolean , null, symbol, bigint
+//non primitive datatype(reference datatypye): array , funtion , objects
+
+// const score = 100;
+// const scorevalue =9.8 ;
+// console.log(scorevalue);
+
+// const isloggedin = true ;
+// const outsidetemp = null ;
+// console.log(outsidetemp);
+
+// let useremail ;//undefined 
+// const id = symbol('123');
+// const anotherid = symbol('4567');
+
+// console.log(id === anotherid);
+// const Bignumber = 13346329857409840975n;
+// console.log(Bignumber);
+// console.log(typeof Bignumber);
+
+// not primitive datatype eaxmple
+//array
+// const fruit = ["apple" , "mango" , "orange"]
+// console.log(fruit);
+
+//objects use key value in curley brackets pair
+ 
+// let mybook = {
+//  book1 : "english" ,
+//  book2 : "urdu" ,
+//  book3 : "math" ,
+//  book4 : "comp" ,
+// } 
+
+// console.log(mybook);
+
+//function 
+const myfunction = function(){
+    console.log("hello world");
+}
