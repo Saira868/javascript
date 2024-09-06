48917fad396682e7c04c94ec2c52fc29738247e0
Saira868 <137001691+Saira868@users.noreply.github.com>
2024-09-06 13:44:28 +0000

    array

---
+++ js_basics/math_number.js
@@ -0,0 +1,45 @@
+const score = 100 ;
+// console.log(score);
+
+const account = new Number(200);// new String(200) [String: '200']
+// console.log(account);
+// console.log(account.toString().length);
+
+// console.log(math);
+// console.log(Math.abs(-2909090));// -ve number convert in +ve number
+// console.log(Math.round(8.8));// 9
+// console.log(Math.min(88 , 99 ,1 , 89));//small number 
+// console.log(Math.max(88 , 99 ,1 , 89));//large number
+
+//date and time ++++++++
+
+// let mydate = new Date();
+// console.log(mydate);// 2024-09-06T09:05:49.783Z
+// console.log(mydate.toString());//Fri Sep 06 2024 09:07:42 GMT+0000 (Coordinated Universal Time)
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toISOString()); //2024-09-06T09:09:37.021Z
+// console.log(mydate.toTimeString());
+// console.log( typeof mydate);
+
+// let createdate = new date("19 , 3 , 2024");
+// console.log(createdate.toLocalString()) ;
+ 
+//Array ++++++++++
+
+// const array = [0 , 2 ,3, 4 , 5 ];
+
+// const array2 = [0 , "d" ,"c", "b", "a" ];
+// // console.log(array[4]);
+// // console.log(array.push("abc"));
+//  const newarray = [...array, ... array2];
+// //  console.log(array2);
+
+// console.log(Array.from("championchamp"));// [c ,h ,a ,m ,p,i,o,n.....]
+// console.log(Array.isarray("champ"));
+
+let a = 5 ;
+let b =10 ;
+let c = 15 ;
+
+console.log(Array.of(a , b, c)); //[ 5, 10 ,15]
+
