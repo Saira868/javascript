dd26486b270f5ddc72c6682a2e19cfe65de1f49d
Saira868 <137001691+Saira868@users.noreply.github.com>
2024-09-12 13:29:01 +0000

    scope

---
+++ js_basics/scope.js
@@ -0,0 +1,20 @@
+function one(){
+    const username = "aira"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    // two()
+}
+// one()
+
+if(true){
+    const username = "aira"
+    if(username === "aira"){
+        const website = "youtube"
+        console.log(username + website) ;
+    }
+    // console.log(website);
+}
+// console.log(username);
